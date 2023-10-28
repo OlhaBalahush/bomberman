6747938e47d9b70f250cec3e7ada68a7c1147f84
Karme Barg <karme.barg@gmail.com>
2023-10-28 13:24:59 +0000

    rebase

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,13 +2,13 @@ import http from 'http';
 import express from 'express';
 import { initWsServer } from './webSockets'
 
-const PORT = 8050;
+const PORT = 8080;
 
 const app = express();
 const HTTPserver = http.createServer(app);
 
 HTTPserver.listen(PORT, () => {
-    console.log('Server is listening on port 8050');
+    console.log('Server is listening on port 8080');
 });
 
 initWsServer(HTTPserver);
