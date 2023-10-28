65eb1f7d5df1db968a189ee8a7750d2f9bf86317
khaletska <khaletska.e@gmail.com>
2023-10-28 16:13:50 +0300

    player class

--- frontend/src/models/player.ts
+++ frontend/src/models/player.ts
@@ -1,7 +1,76 @@
+import { v4 as uuidv4 } from 'uuid';
+
 class Player {
-    username: string
-    lives: 0 | 1 | 2 | 3
-    position: [number, number]
-    isAlive: boolean
-    // add more if needed
+    private _token: string
+    private _username: string
+    private _lives: number
+    private _powerups: {
+        maxBombCount: number
+        explosionRange: number
+        speed: number
+    }
+    private _position: { x: number, y: number }
+
+    constructor(username: string) {
+        this._token = uuidv4()
+        this._username = username
+        this._lives = 3
+        this._powerups = {
+            maxBombCount: 1,
+            explosionRange: 3,
+            speed: 1
+        }
+        // TODO adjust to have different position for different players
+        this._position = { x: 0, y: 0 }
+    }
+
+    // made token, not sure if needed
+    get token(): string {
+        return this._token
+    }
+
+    get username(): string {
+        return this._username
+    }
+
+    get lives(): number {
+        return this._lives
+    }
+
+    set lives(value: number) {
+        this._lives = value
+    }
+
+    get maxBombCount(): number {
+        return this._powerups.maxBombCount
+    }
+
+    addMaxBombCount(count: number): void {
+        this._powerups.maxBombCount += count;
+    }
+
+    get explosionRange(): number {
+        return this._powerups.explosionRange
+    }
+
+    addExplosionRange(range: number): void {
+        this._powerups.explosionRange += range;
+    }
+
+    get speed(): number {
+        return this._powerups.speed
+    }
+
+    addSpeed(speed: number): void {
+        this._powerups.speed += speed;
+    }
+
+    get position(): { x: number; y: number } {
+        return this._position
+    }
+
+    setPosition(x: number, y: number): void {
+        this._position.x = x;
+        this._position.y = y;
+    }
 }
