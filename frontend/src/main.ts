9acf9342afdd8deffe6f886f57c8d96bb970b04c
GitGert <111555089+GitGert@users.noreply.github.com>
2023-11-06 12:13:56 +0200

    Bom 34 lobby bugfix (#10)
    * ws progress - handling events related to initial connect, player creation, lobby creation
    
    * lobby timer progress
    
    * lobby timer fix, added logic to stop timer if player count drops below 2
    
    * removed test code remainings
    
    * variable names with ...id changed to ...ID
    
    * connected lobby FE with Be
    
    * cleaned up the code
    
    ---------
    
    Co-authored-by: Karme Barg <karme.barg@gmail.com>

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -6,16 +6,16 @@ import { gameView } from "./views/gameView";
 
 //add your route here
 const Routes = {
-    "/": usernameView(),
-    "/waiting-room": lobbyView(),
-    "/error": errorView(),
-    "/game": gameView()
+    "/": usernameView,
+    "/waiting-room": lobbyView,
+    "/error": errorView,
+    "/game": gameView
 }
 
 const currentURL = document.location.pathname
 
 if (Routes[currentURL]) {
-    document.body.appendChild(Routes[currentURL].element);
+    document.body.appendChild(Routes[currentURL]().element);
 } else {
-    document.body.appendChild(Routes['/error'].element);
+    document.body.appendChild(Routes['/error']().element);
 }
--- frontend/src/views/lobbyView.ts
+++ frontend/src/views/lobbyView.ts
@@ -2,51 +2,38 @@ import { createDOMElement, useStateManager } from "mini-framework";
 
 export const lobbyView = () => {
 
-    let playerCountInLobby = useStateManager("1")
-    let TimerCountDown = useStateManager("0")
-
-    //TODO: change this to the correct port and and endpoint
-    fetch('https://localhost:8080/GetLobbyCount')
-        .then(response => response.json)
-        .then(data => {
-            console.log(data);
-            //TODO save the gotten number to the playercountInLobby variable
-
-        })
-        .catch(err => console.log(err));
-
     const socket = new WebSocket("ws://localhost:8080/ws")
 
-
     socket.addEventListener('open', (event) => {
-        console.log('WebSocket connection is open:', event);
-
-        //TODO: send a message to backenc with the Players username
-
-        const PlayersUsername = sessionStorage.getItem('username');
-
-        if (PlayersUsername) {
-            socket.send(JSON.stringify({ Username: PlayersUsername }));
-        } else {
-            socket.send(JSON.stringify({ error: "error getting players username" }))
-        }
-
+        console.log('WebSocket connection has been established');
     });
 
-
-
-
-    //TODO make sure that everything here works properly because currently it is full of placeholders
     socket.addEventListener('message', (event) => {
-        console.log('WebSocket message received:', event);
+        const messageData = JSON.parse(event.data)
 
-        switch (event.type) {
+        switch (messageData.type) {
+            case "connect":
+                const clientId = messageData.clientID;
+                sessionStorage.setItem("clientID", clientId)
+
+                const username = sessionStorage.getItem("username")
+                socket.send(JSON.stringify({ "type": "enterLobby", "clientID": clientId, "username": username }))
+
+                break;
+
+            case "enterLobby":
+                const playerCountContainer = document.getElementById("playerCount")
+                const playerCount = messageData.playerCount
+
+                if (playerCountContainer) {
+                    playerCountContainer.innerText = playerCount + " user(s) in the lobby right now"
+                }
+
+                break;
+
             case "startTwentySecondTimer":
-
-                TimerCountDown.setState(event.data)
-
+                const timerSeconds = messageData.seconds
                 const fristTimerText = document.getElementById("firstTimer")
-
                 let isHidden = true;
 
                 if (fristTimerText) {
@@ -57,22 +44,32 @@ export const lobbyView = () => {
                     fristTimerText.classList.toggle('hidden');
                 }
 
-                if (!isHidden && event.data === "0" && fristTimerText) {
-                    fristTimerText.classList.toggle('hidden');
+                if (fristTimerText) {
+                    fristTimerText.innerText = "the countdown will begin in " + String(timerSeconds) + " seconds"
                 }
-                //TODO add logic here to show the countdown from 20 with message:
-                // "20 seconds untill the countdown"
-
 
                 break
 
             case "startTenSecondTimer":
-
-                TimerCountDown.setState(event.data)
+                const timerseconds = messageData.seconds
 
                 const secondTimer = document.getElementById("secondTimer")
 
+                const previousTimer = document.getElementById("firstTimer")
+
                 let isSecondHidden = true;
+
+                if (previousTimer && !previousTimer.classList.contains("hidden")) {
+                    previousTimer.classList.toggle("hidden")
+                }
+
+                //if there are less than 2 people in the lobby all of a sudden:
+                if (messageData.seconds === -1) {
+                    if (secondTimer && !secondTimer.classList.contains('hidden')) {
+                        secondTimer.classList.toggle('hidden');
+                    }
+                    break
+                }
 
                 if (secondTimer) {
                     isSecondHidden = secondTimer.classList.contains('hidden');
@@ -82,14 +79,20 @@ export const lobbyView = () => {
                     secondTimer.classList.toggle('hidden');
                 }
 
-                //TODO add logic here to show the countdown from 10 seconds with the message:
-                // "ten seconds untill game will start..."
-
+                if (secondTimer) {
+                    secondTimer.innerText = "the game will start in " + String(timerseconds) + " seconds"
+                }
 
                 break
 
+            case "startGame": {
+                //todo init game from here
+                //most likely just redirect, but maybe something else needs to be done as well
+                window.location.href = "http://localhost:8080/game"
+            }
+
             default:
-                console.log("error unknow ws connection message type: ", event.type)
+                console.log("error unknow ws connection message type: ", messageData.type)
         }
     });
 
@@ -109,14 +112,13 @@ export const lobbyView = () => {
         class: "min-h-screen flex items-center justify-center bg-neutral-600"
     }, [createDOMElement("div", { class: "flex-column  bg-neutral-200  p-12 w-[600px] h-[400px] shadow-md border-4 border-black text-center" }, [
         createDOMElement("div", { class: "font-mono text-6xl font-normal text-black uppercase" }, ["Waiting Room"]),
-        createDOMElement("div", { class: "pt-7 font-mono text-1xl font-normal text-black uppercase" }, [playerCountInLobby.getState() + " user(s) in the lobby right now"]),
+        createDOMElement("div", { class: "pt-7 font-mono text-1xl font-normal text-black uppercase", id: "playerCount" }, []),
         createDOMElement("div", { class: "py-3 font-mono text-1xl font-normal text-black uppercase" }, ["the game will start when at least 2 players are in the lobby"]),
         createDOMElement("div", { class: "py-4 flex items-center justify-center" }, [
             createDOMElement("img", { src: "https://i.gifer.com/ZKZg.gif", class: "self-center w-[100px] h-[100px]" }, [])
         ]),
-        createDOMElement("div", { class: "hidden font-mono text-1xl font-normal text-black uppercase", id: "firstTimer" }, ["the game will start in " + TimerCountDown.getState() + " second"]),
-        createDOMElement("div", { class: "hidden font-mono text-1xl font-normal text-black uppercase", id: "secondTimer" }, ["the countdown will begin in " + TimerCountDown.getState() + " seconds"])
-        // temp
+        createDOMElement("div", { class: "hidden font-mono text-1xl font-normal text-black uppercase", id: "firstTimer" }, []),
+        createDOMElement("div", { class: "hidden font-mono text-1xl font-normal text-black uppercase", id: "secondTimer" }, [])
     ])])
 
     return HTML
